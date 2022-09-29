b297fe12fa46107ab2e9d0a2608a94c0bf3a055f
Mehdi B <elmehdi.baha@gmail.com>
2022-09-29 14:14:39 +0200

    Update macos_install.sh

--- os/macos_install.sh
+++ os/macos_install.sh
@@ -4,7 +4,7 @@
 ##### Config Variables
 ###########################
 BREW_TAPS=(heroku/brew mongodb/brew)
-BREW_RECIPES=(bash coreutils git mongodb-community git-extras wget mas bat heroku mongodb openssl python trash awscli logitech-options)
+BREW_RECIPES=(bash coreutils git mongodb-community git-extras wget mas bat heroku openssl python trash awscli logitech-options)
 BREW_CASKS=(spectacle vlc firefox google-drive spotify visual-studio-code tunnelblick itsycal)
 MAC_APPS=()
 
