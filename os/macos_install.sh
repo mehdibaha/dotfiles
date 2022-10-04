1a87b41ee526c74c97ac3b2b5d72a43ee8c43167
Mehdi B <elmehdi.baha@gmail.com>
2022-10-05 01:27:31 +0200

    Update macos_install.sh

--- os/macos_install.sh
+++ os/macos_install.sh
@@ -3,9 +3,9 @@
 ###########################
 ##### Config Variables
 ###########################
-BREW_TAPS=(homebrew/cask-drivers heroku/brew mongodb/brew)
-BREW_RECIPES=(bash coreutils git mongodb-community git-extras wget mas bat postgresql heroku openssl python trash awscli logitech-options)
-BREW_CASKS=(rectangle vlc firefox google-drive spotify visual-studio-code tunnelblick itsycal launchcontrol)
+BREW_TAPS=(homebrew/cask-drivers heroku/brew)
+BREW_RECIPES=(bash coreutils git git-extras wget mas bat postgresql heroku openssl python trash awscli logitech-options)
+BREW_CASKS=(rectangle vlc firefox google-drive visual-studio-code tunnelblick itsycal)
 MAC_APPS=()
 
 ###########################
